c8a14ce8cd15ac649d7d19434fd9148ee873e767
jananimohan44 <janani.m@workboosterai.com>
2026-09-14 00:57:50 +0530

    fix: use REG_EXPAND_SZ for powershell command path resolution

--- public/setup-launcher.bat
+++ public/setup-launcher.bat
@@ -26,7 +26,7 @@ echo } >> "%USERPROFILE%\.cncvault\launcher.ps1"
 echo Registering cncvault:// protocol for current user...
 reg add HKCU\SOFTWARE\Classes\cncvault /t REG_SZ /d "URL:CNC Vault Protocol" /f >nul
 reg add HKCU\SOFTWARE\Classes\cncvault /v "URL Protocol" /t REG_SZ /d "" /f >nul
-reg add HKCU\SOFTWARE\Classes\cncvault\shell\open\command /t REG_SZ /d "powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File \"%%USERPROFILE%%\.cncvault\launcher.ps1\" \"%%1\"" /f >nul
+reg add HKCU\SOFTWARE\Classes\cncvault\shell\open\command /t REG_EXPAND_SZ /d "powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File \"%%USERPROFILE%%\.cncvault\launcher.ps1\" \"%%1\"" /f >nul
 
 echo.
 echo =========================================
